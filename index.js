0e778bce2297f5c031a1987ba36bee50b9926b9f
MEKH M T <86202146+mekhmt@users.noreply.github.com>
2024-01-19 13:31:10 +0530

    cors update to http

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 //app.use(cors());
 app.use(cors({
-  origin: 'https://54.164.80.165',
+  origin: 'http://54.164.80.165',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 }));
